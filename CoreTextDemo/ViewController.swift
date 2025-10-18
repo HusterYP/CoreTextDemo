4e817e94d03e6d2e22de164603bb06e4cb503415
ping <1772662957@qq.com>
2025-10-18 17:01:50 +0800

    Initial Commit

---
+++ CoreTextDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreTextDemo
+//
+//  Created by 袁平 on 10/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
